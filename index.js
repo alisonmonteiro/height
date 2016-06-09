00e63cf5dacecbd40cd5fcce4c130f97b5a837cd
Alison <alisonmonteiro.10@gmail.com>
2016-06-09 20:10:12 -0300

    Refatoring

--- index.js
+++ index.js
@@ -3,19 +3,6 @@ var Height = function(opts) {
 
   opts = opts || {};
 
-  var getMaxHeight = function(elements) {
-    var style, heights = [];
-
-    var heights = elements.map(function(item) {
-      style = window.getComputedStyle(item);
-
-      if (heights.indexOf(parseInt(style.height)) !== 0)
-        return parseInt(style.height);
-    });
-
-    return Math.max.apply(Math, heights);
-  }
-
   var checkOpts = function() {
     if (typeof opts.columns !== 'number') {
       throw new TypeError('opts.columns must be a number.')
@@ -34,21 +21,34 @@ var Height = function(opts) {
     elements = Array.prototype.slice.call(elements)
 
     if (elements.length > 0) {
-      for (var count = 0; count < elements.length; count++) {
-        var toPosition = (count + opts.columns - 1);
+      for (var index = 0; index < elements.length; index++) {
+        var toPosition = (index + opts.columns - 1);
 
         toPosition = (toPosition > elements.length)
           ? (elements.length - 1)
           : toPosition;
 
-        rowElements = elements.slice(count, (toPosition + 1));
+        rowElements = elements.slice(index, (toPosition + 1));
 
         maxHeight = getMaxHeight(rowElements);
         setElementsHeight(rowElements, maxHeight);
 
-        count = toPosition;
+        index = toPosition;
       }
     }
+  }
+
+  var getMaxHeight = function(elements) {
+    var style, heights = [];
+
+    heights = elements.map(function(item) {
+      style = window.getComputedStyle(item);
+
+      if (heights.indexOf(parseInt(style.height)) !== 0)
+        return parseInt(style.height);
+    });
+
+    return Math.max.apply(Math, heights);
   }
 
   var setElementsHeight = function(elements, maxHeight) {
