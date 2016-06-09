4c4c9200cce09a56be0c3f0ce47225ef79f036da
Alison <alisonmonteiro.10@gmail.com>
2016-06-09 00:02:58 -0300

    Keep it simple

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ var Height = function(opts) {
 
   opts = opts || {};
 
-  var getMaxHeight = function(elements, begin, end) {
+  var getMaxHeight = function(elements) {
     var style, heights = [];
 
-    if (elements.length > 0) {
-      for (var i = begin; i <= end; i++) {
-        style = window.getComputedStyle(elements[i]);
+    var heights = elements.map(function(item) {
+      style = window.getComputedStyle(item);
 
-        if (heights.indexOf(parseInt(style.height)) !== 0)
-          heights.push(parseInt(style.height));
-      }
-    }
+      if (heights.indexOf(parseInt(style.height)) !== 0)
+        return parseInt(style.height);
+    });
 
     return Math.max.apply(Math, heights);
   }
@@ -29,26 +27,33 @@ var Height = function(opts) {
   }
 
   var init = function() {
-    var elements, maxHeight = 0;
+    var elements = document.querySelectorAll(opts.listElem)
+    var maxHeight = 0;
+    var rowElements = [];
 
-    elements = document.querySelectorAll(opts.listElem)
+    elements = Array.prototype.slice.call(elements)
 
     if (elements.length > 0) {
       for (var count = 0; count < elements.length; count++) {
         var toPosition = (count + opts.columns - 1);
 
-        if (toPosition > elements.length) {
-          toPosition = (elements.length - 1)
-        }
+        toPosition = (toPosition > elements.length)
+          ? (elements.length - 1)
+          : toPosition;
 
-        maxHeight = getMaxHeight(elements, count, toPosition);
+        rowElements = elements.slice(count, (toPosition + 1));
 
-        for (var j = count; j <= toPosition; j++) {
-          elements[j].style.height = maxHeight + 'px';
-        }
+        maxHeight = getMaxHeight(rowElements);
+        setElementsHeight(rowElements, maxHeight);
 
         count = toPosition;
       }
+    }
+  }
+
+  var setElementsHeight = function(elements, maxHeight) {
+    for (var j = 0; j < elements.length; j++) {
+      elements[j].style.height = maxHeight + 'px';
     }
   }
 
